35e277a7c995ff2091f5ee3e731c7d9f837d60c9
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-10-24 17:42:30 +0300

    Cleanup

--- lib/mvc/controllers/FormController.class.php
+++ lib/mvc/controllers/FormController.class.php
@@ -4,16 +4,24 @@ class FormController extends ViewController
 {
 	public function onSubmit($actId, $phase, $targetGId, $httpInput, $targetHttpInput)
 	{
-		echo 'SUBMITTING';
+		return;
+		//echo 'da';
+		//print_r($httpInput);
+		//return;
+		//echo 'SUBMITTING';
 		//echo '<pre>';
 		//print_r($_POST);
 		//$this->data->relationshipPath->debugPrintData();
-		$this->data->import($_POST['data'], array(\alib\model\RelationshipPath::IDX_LOADED => true));
-		echo 'AFTER';
-		//$this->data->relationshipPath->debugPrintData();
+		//print_r($this->data->contentItems->items()); die();
+		//echo count($this->data->contentItems->items());die();
+		$this->data->import($_POST['contentItemController']['data'], array(\alib\model\RelationshipPath::IDX_LOADED => true));
+		//echo 'AFTER';
+		
+		//$this->data->relationshipPath->contentItems;
+		//print_r($this->data->relationshipPath->paths); die('000');
 		//die();
 		//return;
-		$GLOBALS['dbg']=true;
+		//$GLOBALS['dbg']=true;
 		//echo '<pre>';
 		try
 		{
@@ -25,11 +33,12 @@ class FormController extends ViewController
 		{
 			
 			//echo 'axxa'.$err->getCode();
-			$this->view->setErrorCodes($err->getErrorCodes());
+			//$this->view->setErrorCodes($err->getErrorCodes());
 			//$err->throwIfErrors();
 		}
-		$GLOBALS['dbg']=false;
-		
+		//$GLOBALS['dbg']=false;
+		//echo "AFTER SAVE\n";
+		//echo '</pre>';
 //		/echo '</pre>';
 		
 		//echo 'rpk: '.$this->data->rpk;
